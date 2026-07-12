a84aa392c23545d2755056f808a833ddfc6e0bde
prabhas-koppireddy <prabhaskoppireddy@gmail.com>
2026-07-12 19:01:50 +0530

    URL updated in cors

--- server/server.ts
+++ server/server.ts
@@ -21,7 +21,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', "https://thumblify-ai-seven.vercel.app"],
     credentials: true
 }))
 
